2c789eb441527f48e107460e9427af83055c154f
Jim Bullington <jimb@jrbcs.com>
2011-06-16 12:55:17 -0400

    Issue #1136044 by jimbullington: Add 4 competitor DE and MDE brackets

---
+++ designs/mde4champ/bracket-mde4champ.tpl.php
@@ -0,0 +1,50 @@
+<?php
+// $Id:
+
+/**
+ * @file
+ * Template for mde4champ bracket design
+ *
+ * Available variables:
+ * - $node: The node object for the bracket to be rendered.
+ */
+drupal_add_css(drupal_get_path('module', 'bracket') . '/designs/mde4champ/bracket-mde4champ.css', 'theme', 'all', FALSE);
+?>
+
+<div class="bracket-block">
+  <?php if ($node->subtitle) { ?>
+  <div class="bracket-subtitle"><?php print filter_xss_admin($node->subtitle) ?></div>
+  <?php } ?>
+  <?php if ($node->comments) { ?>
+  <div class="bracket-comments"><?php print nl2br(filter_xss_admin($node->comments)) ?></div>
+  <?php } ?>
+  <div id="round-1" class="bracket-round">
+    <div class="bracket-round-comment"><?php print $node->round[1]->comment ? filter_xss_admin($node->round[1]->comment) : '&nbsp;' ?></div>
+    <div id="round-1-spacer"></div>
+    <?php print theme('bracket_match', $node, 1, 1, 'left') ?>
+    <div class="bracket-round-1-match-spacer"></div>
+    <?php print theme('bracket_match', $node, 1, 2, 'left') ?>
+    <div id="round-1-loser-round-spacer"></div>
+    <?php print theme('bracket_match', $node, 3, 1, 'left') ?>
+    <div class="bracket-footer"><?php print nl2br(filter_xss_admin($node->footer)) ?></div>
+  </div>
+  <div id="round-2" class="bracket-round">
+    <div class="bracket-round-comment"><?php print $node->round[2]->comment ? filter_xss_admin($node->round[2]->comment) : '&nbsp;' ?></div>
+    <div id="round-2-spacer"></div>
+    <?php print theme('bracket_match', $node, 2, 1, 'left') ?>
+    <div id="round-2-loser-round-spacer"></div>
+    <?php print theme('bracket_match', $node, 4, 1, 'left') ?>
+  </div>
+  <div id="results" class="bracket-results bracket-results-left">
+    <div id="results-spacer"></div>
+    <?php print theme('bracket_result', $node, 1, 'left') ?>
+    <div class="bracket-result-spacer"></div>
+    <?php print theme('bracket_result', $node, 2, 'left') ?>
+    <div id="loser-results-spacer"></div>
+    <?php print theme('bracket_result', $node, 3, 'left') ?>
+    <div class="bracket-result-spacer"></div>
+    <?php print theme('bracket_result', $node, 4, 'left') ?>
+  </div>
+  <?php print theme('bracket_images', $node) ?>
+</div>
+
